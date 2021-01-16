73006daf290ef019ad5b676b8cedc3a3d8666684
songlongwei <1210520916@qq.com>
2021-01-16 15:56:07 +0800

    准备好路由页面了

--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import ElementUI from 'element-ui'// 引入element-ui
+import 'element-ui/lib/theme-chalk/index.css'
+import '../src/styles/index.less'
+
+Vue.use(ElementUI)// 全局注册Elementui
 
 Vue.config.productionTip = false
 
